ed54b3dac522e3dc3086f7d1b9c9729ee20de890
0ddlyoko <0ddlyokoOfficial@gmail.com>
2020-02-25 08:24:12 +0100

    Fix motd & pom.xml

--- src/main/java/com/froxynetwork/froxybungee/websocket/commands/ServerRegisterCommander.java
+++ src/main/java/com/froxynetwork/froxybungee/websocket/commands/ServerRegisterCommander.java
@@ -12,21 +12,20 @@ import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.config.ServerInfo;
 
 /**
- * FroxyBungee
- * Copyright (C) 2019 FroxyNetwork
+ * FroxyBungee Copyright (C) 2019 FroxyNetwork
  * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
  * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
  * 
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
  *
  * @author 0ddlyoko
  */
@@ -52,7 +51,7 @@ public class ServerRegisterCommander implements IWebSocketCommander {
 	public void onReceive(String from, String message) {
 		// message = <serverId | host | port | motd>
 		String[] split = message.split(" ");
-		if (split.length != 4) {
+		if (split.length < 4) {
 			// Error
 			LOG.error("Invalid message: {}", message);
 			return;
@@ -61,7 +60,8 @@ public class ServerRegisterCommander implements IWebSocketCommander {
 		String host = split[1];
 		String strPort = split[2];
 		int port = -1;
-		String motd = split[3];
+		// 3 = 3 spaces
+		String motd = message.substring(split[0].length() + split[1].length() + split[2].length() + 3);
 		try {
 			port = Integer.parseInt(strPort);
 		} catch (NumberFormatException ex) {
